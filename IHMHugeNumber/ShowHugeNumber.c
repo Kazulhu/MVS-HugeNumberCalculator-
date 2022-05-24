f4d87f6bbba7fb1d052738a0ca8c79bf124a7463
Fady <fady.mechlaoui.pro@gmail.com>
2022-05-24 10:41:11 +0200

    Modified the ShowHugeNumber.c file and added to it the printHugeInt() function

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -3,3 +3,9 @@ void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
     printf ("%s\n", hugeUnsignedIntString);
     free (hugeUnsignedIntString);
 }
+
+void printHugeInt (const HugeInt* hugeInt) {
+    char* hugeIntString = HugeIntToString (hugeInt);
+    printf ("%s\n", hugeIntString);
+    free (hugeIntString);
+}
